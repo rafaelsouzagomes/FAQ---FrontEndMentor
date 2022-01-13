957e172e5489216042d4025e2afb7d09861294c1
rafaelsouzagomes <rafaelsouzagomes13@gmail.com>
2022-01-12 21:23:48 -0300

    layout FAQ

---
+++ main.js
@@ -0,0 +1,52 @@
+
+function OpenOrCloseQuestion(questionName, arrowId){
+
+    const question = document.getElementById(questionName);
+    const arrow = document.getElementById(arrowId);
+    
+    var paragraphs = document.getElementsByTagName("p");
+    console.log(paragraphs);
+
+    var firstColumn = document.getElementsByClassName("hidden");
+    console.log(firstColumn);
+
+    if(question.classList.contains('hidden')){
+        question.classList.remove('hidden');
+        arrow.classList.add('arrow-rotated');
+        ocultarTodasPerguntasExceto(questionName);
+    }
+    else{
+        question.classList.add('hidden');
+        arrow.classList.remove('arrow-rotated');
+    }
+}
+
+
+
+function ocultarTodasPerguntasExceto(questionName){
+
+    const question1 = document.getElementById('firstQuestion');
+    const question2 = document.getElementById('secondQuestion');
+    const question3 = document.getElementById('thirdQuestion');
+    const question4 = document.getElementById('fourthQuestion');
+    const question5 = document.getElementById('fifthQuestion');
+
+    if(questionName!=='firstQuestion'){
+        question1.classList.add('hidden');
+    }
+    
+    if(questionName!=='secondQuestion'){
+        question2.classList.add('hidden');
+    }
+
+    if(questionName!=='thirdQuestion'){
+        question3.classList.add('hidden');
+    }
+
+    if(questionName!=='fourthQuestion'){
+        question4.classList.add('hidden');
+    }
+    if(questionName!=='fifthQuestion'){
+        question5.classList.add('hidden');
+    }
+}
