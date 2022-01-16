f649ccdd1480387a0e88500088e06a9aec789530
rafaelsouzagomes <rafaelsouzagomes13@gmail.com>
2022-01-15 22:31:58 -0300

    finalizando versão inicial

--- main.js
+++ main.js
@@ -25,22 +25,37 @@ function ocultarTodasPerguntasExceto(questionName){
     const question4 = document.getElementById('fourthQuestion');
     const question5 = document.getElementById('fifthQuestion');
 
+    const arrow1 = document.getElementById('arrowFirstQuestion');
+    const arrow2 = document.getElementById('arrowSecondQuestion');
+    const arrow3 = document.getElementById('arrowThirdQuestion');
+    const arrow4 = document.getElementById('arrowFourthQuestion');
+    const arrow5 = document.getElementById('arrowFifthQuestion');
+
+
+
     if(questionName!=='firstQuestion'){
         question1.classList.add('hidden');
+        arrow1.classList.remove('arrow-rotated');
+
     }
     
     if(questionName!=='secondQuestion'){
         question2.classList.add('hidden');
+        arrow2.classList.remove('arrow-rotated');
     }
 
     if(questionName!=='thirdQuestion'){
         question3.classList.add('hidden');
+        arrow3.classList.remove('arrow-rotated');
     }
 
     if(questionName!=='fourthQuestion'){
         question4.classList.add('hidden');
+        arrow4.classList.remove('arrow-rotated');        
     }
+
     if(questionName!=='fifthQuestion'){
         question5.classList.add('hidden');
+        arrow5.classList.remove('arrow-rotated');
     }
 }
